701b3447b12a05264711b5e790591212d02852a6
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-11-13 22:57:20 -0800

    add reconnect logic

--- static/js/u.js
+++ static/js/u.js
@@ -1,2 +1,7 @@
-function LOG(x){document.body.innerHTML+='<li>'+str(x)+'</li>'}
+function LOG1(x){document.body.innerHTML+='<li>'+str(x)+'</li>'}
+function LOG(x){
+    var elt = document.createElement('li');
+    elt.appendChild(document.createTextNode(x));
+    document.body.appendChild(elt);
+}
 function str(x){return JSON.stringify(x)}
--- static/js/ws.js
+++ static/js/ws.js
@@ -1,3 +1,4 @@
+var WS;
 function ws() {
     var _ws = new WebSocket("ws://localhost:8080/websocket");
     _ws.onopen = function() {
@@ -13,12 +14,17 @@ function ws() {
     };
     _ws.onclose = function () {
 	LOG('CLS100');
-	setTimeout(reconnect,1200);
+	if (WS && WS.reconnectDelay)
+	    setTimeout(reconnect,WS.reconnectDelay);
     };
-    return _ws;
+    return {self:_ws,reconnectDelay:1200};
 }
 function reconnect() {
     LOG('RECONNECT');
     WS=ws();
 }
-window.onload=reconnect;
+function clicky() {
+    LOG('CLICKY');
+    WS.reconnectDelay = 0;
+    WS.self.close();
+}
