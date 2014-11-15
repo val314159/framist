ad4161a85b4909f78e6b91995c0647562a6ba481
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-11-15 14:56:10 -0800

    add channel concept

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,17 @@
+
 function App(cfg) {
     if (this===window) return new App(cfg);
     var self=this;
     LOG('LAUNCH============');
     bindVar(self,'$ws',ws());
-    bindVar(self,'$wu',wu());
-    var _ws = self.$ws();
-    var _wu = self.$wu();
-    _wu.$ws(_ws);
-    _ws.$wu(_wu);
     if (cfg.autoStart) self.$ws().toggleCxn();
     LOG('LAUNCH2============');
+
+    self.say=say(value) {
+	LOG("QQ:"+app.$ws().$name());
+	LOG("Q@:"+value);
+	_ws.send(str([0,'chat','message',
+		      [ app.$ws().$name(), value ]]));
+	LOG("QQ________");
+    };
 }
