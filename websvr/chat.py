a92c4df83ab60fe9386f62126965414aa9dea07d
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-11-19 09:16:26 -0800

    clean up a little

--- websvr/chat.py
+++ websvr/chat.py
@@ -6,50 +6,31 @@ class Chat:
     def connect(wu, sid):
         print "XX CONNECT5", sid
         wu.add_user()
-        d = {"method":"connect","params":wu.dict(),"id":wu.sid()}
-        wu.send(d)
+        wu.send({"method":"connect",
+                 "params":wu.dict(),"id":wu.sid()})
         print "XX CONNECT99999", sid
         pass
     @staticmethod
     def name(wu,name):
         print "XX @ NAME", name
         old_name = wu.name ; wu.name = name
-        d = {"name":{"sid":wu.sid(),"name":name,
-                     "oldName":old_name}}
-        wu.wsend(d)
+        wu.send({"method":"name",
+                 "params":{"from":wu.dict()
+                           "oldName":old_name}})
         pass
     @staticmethod
     def channel(wu,channel):
         print "XX @ CHANNEL", channel
         old_channel = wu.channel ; wu.channel = channel
-        d = {"channel":{"channel":channel,"sid":wu.sid(),
-                        "oldChannel":old_channel}}
-        wu.send(d)
+        wu.send({"method":"channel",
+                 "params":{"from":wu.dict()
+                           "oldChannel":old_channel}})
         pass
     @staticmethod
     def say(wu,msg,channel):
         print "XX @ SAY", msg, channel
         d = {"msg":msg,"channel":channel,"from":wu.dict()}
-        d = {"method":"say","params":d}
-        wu.send(d,channel)
-        pass
-    @staticmethod
-    def yell(wu,msg):
-        print "XX @ YELL", msg
-        d = {"yell":{"sid":wu.sid(),"msg":msg}}
-        wu.send(d)
-        pass
-    @staticmethod
-    def whisper(wu,to,msg):
-        print "XX @ WHISPER", to, msg
-        d = {"whisper":{"sid":wu.sid(),"msg":msg,"to":to}}
-        wu.send(d)
-        pass
-    @staticmethod
-    def sys(wu,data,channel=None):
-        print "XX @ SYS", data
-        d = {"sys":data}
-        wu.send(d,channel)
+        wu.send({"method":"say","params":d},channel)
         pass
     @staticmethod
     def disconnect(wu,data):
