20debf2aefd132bcb7b9e0a95c7f4b22b72f2f1d
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-11-22 15:01:37 -0800

    now real one command nirvana

--- env.sh
+++ env.sh
@@ -1,31 +1,56 @@
 export AUTH_PORT=1234
 export DS_PORT=1236
 
-function freeze() {
+freeze() {
   pip freeze >requirements.txt
 }
 
-function run_websvr() {
+run_websvr() {
   python -m websvr
 }
 
-function run_authsvr() {
+run_authsvr() {
   zerorpc --server --bind tcp://*:$AUTH_PORT auth.ds.levelds.DS
 }
 
-function run_dssvr() {
+run_dssvr() {
   zerorpc --server --bind tcp://*:$DS_PORT ds_leveldb.DS
 }
 
-function clt_auth_login1() {
+ctrl_c() {
+  echo
+  echo ' *CTRL_C*'
+  echo
+}
+
+pysleep () {
+  python -c"import sys,time;time.sleep(float($1))"
+}
+
+run_both() {
+  trap ctrl_c 2
+  zerorpc --server --bind tcp://*:$AUTH_PORT auth.ds.levelds.DS &
+  python -m websvr &
+  wait
+  kill %1 %2
+  echo waiting 1/2 of a second...
+  pysleep 0.5
+  echo killall python
+  killall -9 python 2>/dev/null
+  pysleep 0.05 # just to let the output sync up
+  echo Done.
+  trap 2
+}
+
+clt_auth_login1() {
   zerorpc --client --connect tcp://127.0.0.1:$AUTH_PORT login_user v pass
 }
 
-function clt_auth_login2() {
+clt_auth_login2() {
   zerorpc --client --connect tcp://127.0.0.1:$AUTH_PORT login_user vx pass
 }
 
-function clt_auth_login3() {
+clt_auth_login3() {
   zerorpc --client --connect tcp://127.0.0.1:$AUTH_PORT login_user v not
 }
 
