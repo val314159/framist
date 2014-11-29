4fc5955e7f860adb56996e38b47a1c80ef1b028c
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-11-29 15:06:55 -0800

    better

--- static/console/appx.js
+++ static/console/appx.js
@@ -1,11 +1,8 @@
 function App(){
     if (this===window) return new App()
-			   //LOG(">> START APP")
     var self=this
     self.id = "app:"+Math.trunc(Math.random()*1000)
-			   //LOG.fmt("ID --> {0} ;",self.id)
     self.execCmd=function(cmdstr){
-	//LOG.fmt("EXEC:{0}", cmdstr)
 	var arr = cmdstr.split(' ')
 	var cmd = arr.shift()
 	var d
@@ -15,13 +12,18 @@ function App(){
 	    LOG.fmt("[[{0}||{1}||{2}]]", cmd, channel, newobj)
 	    d={method:cmd,params:{channel:channel,data:newobj},id:self.id}
 	} else if (startsWith(cmd,'.y')) {
-	    //LOG.fmt("YELL:{0};",str(cmd))
 	    arr.unshift(cmd.substr(2));
 	    var msg = arr.join(' ')
-	    //LOG.fmt("MSG::{0};",msg)
-	    ws.send(str({method:'pub',params:{channel:'y',data:{
-				from:[self.channels[0]],typ:'yell',msg:msg
-				    }}}))
+	    self.pub('y',msg,'yell')
+	} else if (startsWith(cmd,'.p')) {
+	    var who = arr.shift()
+	    var msg = arr.join(' ')
+	    self.pub(who,msg,'whisper')
+	} else if (startsWith(cmd,'\"')) {
+	    var who=self.channels[0];
+	    arr.unshift(cmd.substr(1));
+	    var msg = arr.join(' ')
+	    self.pub(who,msg,'talk')
 	} else if (cmd=='sub') {
 	    LOG.fmt("[[{0}||{1}]]", cmd, str(arr))
 	    var add=[], dlt=[];
@@ -29,31 +31,44 @@ function App(){
 		    if (v[0]!='-')  add.push(v);
 		    else  dlt.push(v.substr(1));
 		});
-	    d={method:cmd,params:{add:add,dlt:dlt},id:self.id}
+	    ws.send(str({method:cmd,params:{add:add,dlt:dlt},id:self.id}))
 	} else if (cmd=='put') {
 	    var key = arr.shift()
 	    var newobj = JSON.parse(arr.join(' '))
 	    LOG.fmt("[[{0}||{1}||{2}]]", cmd, key, newobj)
-	    d={method:cmd,params:{key:key,value:newobj},id:self.id}
+	    ws.send(str({method:cmd,params:{key:key,value:newobj},id:self.id}))
 	} else if (cmd=='get') {
 	    var key = arr.shift()
 	    LOG.fmt("[[{0}||{1}]]", cmd, key)
-	    d={method:cmd,params:{key:key},id:self.id}
+	    ws.send(str({method:cmd,params:{key:key},id:self.id}))
 	} else if (cmd=='range') {
 	    var key0 = arr.shift()
 	    var keyn = arr.shift()
 	    LOG.fmt("[[{0}||{1}||{2}]]", cmd, key0, keyn)
-	    d={method:cmd,params:{key0:key0,keyn:keyn},id:self.id}
+	    ws.send(str({method:cmd,params:{key0:key0,keyn:keyn},id:self.id}))
 	} else {
 	    LOG("UNKNOWN CMD")
 	}
-	if (d) {
-	    LOG.fmt("D:{0}", str(d))
-	    ws.send(str(d))
-	}
     }
+    /////////////////////////////////////////////////////////
     self.onMsg=function(msg){
-	LOG.fmt("ON MSG:{0}", str(msg))
+	if (msg.method=='pub') {
+	    if (msg.params) {
+		var data = msg.params.data
+		if (data) {
+		    if (data.typ=='yell') {
+			LOG.fmt("<b>(*{0}*) {1}</b>", data.from, data.msg)
+		    } else if (data.typ=='talk') {
+			LOG.fmt("({0}) {1}", data.from, data.msg)
+		    } else if (data.typ=='whisper') {
+			LOG.fmt("<b><i>(p,{0}) {1}</i></b>",data.from,data.msg)
+		    } else {
+			LOG.fmt("<b>Unknown Data Type: {0}</b><i>{1}</i>",
+				data.typ, str(data))
+		    }
+		}
+	    }
+	}
     }
     App.onchange = function(x){
 	LOG(x.value)
@@ -61,37 +76,33 @@ function App(){
 	x.value=''
 	self.execCmd(cmd)
     }
+    self.sub=function(add,dlt){
+	ws.send(str({method:'sub',params:{add:add,dlt:dlt}}));
+    }
+    self.pub=function(channel,msg,typ){
+	ws.send(str({method:'pub',
+		     params:{channel:channel,
+			     data:{msg:msg,
+				   from:[self.channels[0]],typ:typ}}}));
+    }
+    self.pub2=function(channel,msg,typ){
+	ws.send(str({method:'pub',
+		     params:{channel:channel,
+			     data:{msg:msg,
+				   from:self.channels,typ:typ}}}));
+    }
+    self.onopen=function(e){
+	LOG(">> Socket opened..."+str(e))
+	self.channels=channels=['n?','c0','.c','a','s','y']
+	self.sub (channels)
+	self.pub2('a',  'Hi everyone','announce')
+	self.pub2('.c', '',           'join')
+	self.pub2('.c0','',           'join')
+	self.pub ('y',  'HI',         'yell')
+    }
     var ws = new WebSocket("ws://localhost:8080/websock?accessToken=vat")
-    ws.onopen=function(e){
-	LOG("XO"+str(e))
-	//LOG("XO=====")
-	self.channels=channels=['n?','c0','.c','s','y']
-	ws.send(str({method:'sub',params:{add:channels}}))
-	ws.send(str({method:'pub',params:{channel:'y',data:{
-			    from:[channels[0]],typ:'yell',msg:'HI'
-				}}}))
-    }
-    ws.onmessage=function(e){
-	//LOG("ONMESSAGE:"+str(e))
-	var j = JSON.parse(e.data)
-	//self.onMsg(j)
-	if (j.method=='pub') {
-	    if (j.params) {
-		var data = j.params.data
-		if (data) {
-		    //LOG("XGOT A REAL PUB" + str(data))
-		    if (data.typ=='yell') {
-			LOG.fmt("<b>(*{0}*) {1}</b>", data.from, data.msg)
-		    }
-		}
-	    }
-	}
-    }
-    ws.onclose=function(e){
-	LOG("XC"+e)
-    }
-    ws.onerror=function(e){
-	LOG("XE"+e)
-    }
-    //LOG(">> END APP")
+    ws.onopen=function(e){self.onopen(e)}
+    ws.onmessage=function(e){self.onMsg(JSON.parse(e.data))}
+    ws.onclose=function(e){LOG("XC"+e)}
+    ws.onerror=function(e){LOG("XE"+e)}
 }
--- websvr/ds.py
+++ websvr/ds.py
@@ -1,13 +1,15 @@
-import json
+import json, leveldb
 def pushv(a,v): a.append(v); return v
 def identity(x): return x
 class DatastoreMixin:
     @staticmethod
-    def DS(__=[]): return(__[0] if __ else pushv(__,LevelDB('.xdb')))
+    def DS(__=[]): return(__[0] if __ else pushv(__,leveldb.LevelDB('.xdb')))
     def h_put(_,key,value,encode=json.dumps):
         _.DS().Put(key,encode(value))
+        print "PUT", key, value
         return dict( method='put', result=None )
     def h_get(_,key,decode=json.loads):
+        print "GET", key, _.DS().Get(key)
         try:return dict( method='get', result=decode(_.DS().Get(key)))
         except KeyError:
             return dict( method='get', error={"message":"Key Not Found"})
