9baf2f7724fb24d03e2bf4351104f0012efb7834
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-12-01 14:31:34 -0800

    start fssvr

---
+++ fssvr/__init__.py
@@ -0,0 +1,45 @@
+from bottle import static_file, abort, Bottle, request
+import json
+app=Bottle()
+@app.route('/')
+def server_static():
+  return static_file('fs/index.html', root='./static')
+@app.route('/ws/fs')
+def r_websock():
+    ws = request.environ.get('wsgi.websocket')
+    if not ws: abort(499,errmsg("Expected WebSocket req."))
+    print "OPENED"
+    while 1:
+      message=ws.receive()
+      print "MESSAGE", repr(message)
+      if not message:
+        break
+      msg=json.loads(message)
+      if msg['method']=='fs_get':
+        print "GETTTT", msg['params'][0]
+        fname = 'fs/'+msg['params'][0]
+        data = open(fname).read()
+        eltName = msg['params'][1]
+        ws.send(json.dumps({"result":[fname,data,eltName]}))
+      elif msg['method']=='fs_put':
+        print "PUTTTT", msg['params'][0], msg['params'][1]
+        fname = 'fs/'+msg['params'][0]
+        data = msg['params'][1]
+        f=open(fname,'w')
+        for x in data:
+          f.write(x)
+          pass
+        pass
+      else:
+        print "UNKNOWN"
+        pass
+      pass
+    print "CLOSED"
+
+###################################
+###################################
+from gevent.pywsgi import WSGIServer
+from geventwebsocket.handler import WebSocketHandler as WSH
+def launch():
+    WSGIServer(('', 8383),handler_class=WSH,
+               application=app).serve_forever()
---
+++ fssvr/__main__.py
@@ -0,0 +1,2 @@
+from . import launch
+if __name__=='__main__':launch()
