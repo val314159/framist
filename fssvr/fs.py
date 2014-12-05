a61fad718d54a729ff0dd562c57d28ece7950350
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-12-04 23:49:49 -0800

    add system command

--- fssvr/fs.py
+++ fssvr/fs.py
@@ -9,18 +9,24 @@ def unescape(s):
     return s
 
 class FilesystemMixin:
-    def h_fs_get(_,path,eltName):
+    def h_fs_get(_,path,eltName=''):
         from stat import S_ISDIR
         data = (escape(open(path).read())
                 if not S_ISDIR(os.stat(path).st_mode)
                 else [(p,S_ISDIR(os.stat(path+'/'+p).st_mode))
                         for p in os.listdir(path)])
-        _.ws.send(json.dumps({"result":[path,data,eltName]}))
+        _.ws.send(json.dumps({"method":"fs_get","result":[path,data,eltName]}))
         pass
     def h_fs_put(_,path,data):
         f=open(path,'w')
         for x in data: f.write(unescape(x))
         f.close()
+        pass
+    def h_fs_system(_,path,eltName='',cwd=None):
+        import subprocess as sp
+        import shlex
+        data=sp.Popen(shlex.split(path),cwd=cwd,stdout=sp.PIPE, stderr=sp.PIPE).communicate()
+        _.ws.send(json.dumps({"method":"fs_system","result":[path,data,eltName]}));
         pass
     def h_fs_mkdir (_,path): os.mkdir(path)
     def h_fs_rmdir (_,path): os.rmdir(path)
