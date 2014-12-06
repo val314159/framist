6de0abf7273ed5515214dc6ff298eed7b7d872c7
Joel "val" Ward <val@Joels-MacBook-Pro.local>
2014-12-06 15:31:12 -0800

    make formatting nicer

---
+++ static/fssvr/console-dashboard.js
@@ -0,0 +1,171 @@
+//<script>
+$(document).keydown(function(evt){
+	if (evt.keyCode==83 && (evt.ctrlKey)){
+	evt.preventDefault();
+	alert('worked1');
+savex();
+alert("worked2");
+/*
+    } else if (evt.keyCode==9) {
+        evt.preventDefault();
+                  document.execCommand("InsertHTML",false,"&#09;");
+*/
+  }
+});
+function LOG(x){console.log(x)}
+var elt = $("#edit");
+ elt.keydown(function(e){
+        if(e.keyCode==9){
+LOG("O NOES " + e.ctrlKey + e.altKey + e.metaKey);
+		if (e.altKey  || e.metaKey) {
+	           e.preventDefault();
+        	   document.execCommand("InsertHTML",false,"&#09;");
+	        }
+	}
+})
+
+
+
+
+
+
+function str(x){return JSON.stringify(x)}
+function $E(x){return document.getElementById(x.substr(1))}
+function $H(x){return $E(x).innerHTML} 
+function $V(x){return $E(x).value} 
+function $I(e,y){e.innerHTML=y;return e} 
+function LOG(x){document.body.appendChild($I(document.createElement("li"),x))}
+function APPEND(e,x){e.appendChild($I(document.createElement("li"),x))}
+var ws = new WebSocket("ws://localhost:8383/ws/fs");
+var getId = function(){
+  var _id=1000;
+  return function(){return _id++;}
+}();
+ws.onopen=function(e){
+  LOG("OPN:"+e);
+  loadx();
+  LOG("OPN2:"+e);
+}
+function send(x){
+  try{
+    ws.send(x)
+  } catch (e) {
+    LOG("SEND ERR:"+e)
+  }
+}
+function go_to2(x){
+  go_to($E("#path").value + '/' + x);
+}
+function go_to(x){    
+    LOG("Go To " +x);
+    $E("#path").value = x;
+    loadx();
+}
+function updir(){
+  go_to2('..');
+}
+function system(){
+	LOG("SYSTEM");
+	var cmd = prompt("Enter directory name to create:","");
+	LOG("SYSTEM:"+str(cmd));
+        var path=$V("#path");
+	fs_system(cmd,"#edit",path);
+}
+function mkdir(){
+LOG("MKDIR");
+  var dirname = prompt("Enter directory name to create:","");
+LOG("MKDIR:"+str(dirname));
+  var path=$V("#path");
+  fs_mkdir(path+'/'+dirname);
+  setTimeout(function(){loadx()},50);
+}
+function touch(){
+LOG("TOUCH");
+  var dirname = prompt("Enter filename to create:","");
+LOG("TOUCH:"+str(dirname));
+  var path=$V("#path");
+  fs_touch(path+'/'+dirname);
+  setTimeout(function(){loadx()},50);
+}
+function rmdir(){
+  LOG("RMDIR");
+  var dirname = prompt("Enter name to remove:","");
+  var path=$V("#path");
+  fs_rmdir(path+'/'+dirname);
+  setTimeout(function(){loadx()},50);
+}
+ws.onmessage=function(x){
+  LOG("MSG:"+x.data);
+  m=JSON.parse(x.data);
+  //LOG(m.result[1]);
+  var text=m.result[1];
+  var elt = $E(m.result[2]);
+  if(m.method=="fs_system"){
+    LOG("ITS A SYSTEM THANG" + str(m.result));
+   elt.innerHTML = text[0] + '<hr>' + text[1];
+  }else if(m.method=="fs_getx") {
+  }else if(typeof(text)=="string"){
+    text = text.replace(/  /g,' &nbsp;');
+    text = text.replace(/\n/g,'<br>');
+    elt.innerHTML = text;
+    elt.contentEditable=true;
+    elt.onkeydown=function(e){
+        if(e.keyCode==9){
+           e.preventDefault();
+           document.execCommand("InsertHTML",false,"&#09;");
+        }
+    }
+  }else{
+    elt.innerHTML = "";
+//    var s0 = '<a onclick="touch()">touch</a>';
+//    var s1 = '<a onclick="mkdir()">mkdir</a>';
+//    var s2 = '<a onclick="rmdir()">rmdir</a>';
+//    APPEND(elt,s0);
+//    APPEND(elt,s1);
+//    APPEND(elt,s2);
+    var s3 = '<a onclick="updir()">..</a>';
+APPEND(elt,s3);
+    for (var n=0;n<text.length;n++) {
+      var v=text[n][0];
+      var v2=v;
+      if (text[n][1])
+	  v2 += '/';
+      var s = "<a xhref='/f?path="+v+"' onclick=go_to2('"+v+"') >"+v2+"</a>";
+      APPEND(elt,s);
+    }
+    elt.contentEditable=false;
+  }
+}
+ws.onclose=function(x){  LOG("CLS:"+x); }
+ws.onerror=function(x){  LOG("ERR:"+x); }
+
+function fs_get(path,eltName){
+  send(str({"method":"fs_get","params":[path,eltName]}))}
+function fs_put(path,data){
+  send(str({"method":"fs_put","params":[path,data]}))}
+function fs_mkdir(path){
+  send(str({"method":"fs_mkdir","params":[path]}))}
+function fs_system(path,eltName,cwd){
+  send(str({"method":"fs_system","params":[path,eltName,cwd]}))}
+function fs_rmdir(path){
+  send(str({"method":"fs_rmdir","params":[path]}))}
+function fs_touch(path){
+  send(str({"method":"fs_touch","params":[path]}))}
+function fs_unlink(path){
+  send(str({"method":"fs_unlink","params":[path]}))}
+function savex(){
+	var text = $H("#edit");
+	text = text.replace(/\<\/div\>/g,'');
+	text = text.replace(/\<div\>/g,'\n');
+	text = text.replace(/\<br\>/g,'\n');
+	text = text.replace(/&nbsp;/g,' ');
+	fs_put($V("#path"),[text]); }
+function loadx(){ fs_get($V("#path"),"#edit"); }
+function rebootx(){
+	var cfg = {url: 'http://localhost:8282/stop'};
+	$.ajax(cfg).then(function(){
+		LOG("YES");
+	},function(){
+		LOG("NO");
+	})}
+//</script>
